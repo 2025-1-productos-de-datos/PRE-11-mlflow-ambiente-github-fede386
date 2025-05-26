749ffe39aa1a53b5e69de5c76bac2145df09cb94
fede386 <49293629+fede386@users.noreply.github.com>
2025-05-26 13:59:44 -0500

    Solucion

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,5 +1,32 @@
+"""Grading"""
+
 import os
+import subprocess
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def test_01():
-    assert os.path.exists("MLproject.txt"), "El archivo MLproject no existe."
+
+    # Test if the homework script runs without errors
+    try:
+        subprocess.run(
+            [
+                "bash",
+                "run.sh",
+                # "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
+            ],
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Error running the homework script: {e}")
+
+    # Ensure the mlruns directory exists
+    assert os.path.exists("mlruns"), "mlruns directory does not exist."
+
+    # Check if there are any experiments saved in mlruns/
+    experiments = [
+        d for d in os.listdir("mlruns") if os.path.isdir(os.path.join("mlruns", d))
+    ]
+    assert len(experiments) > 0, "No experiments found in mlruns directory."
